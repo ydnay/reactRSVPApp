4f97c3ff3968944bb3b7189f4b9bacfc74d02ec9
Yandy Gonzalez <ygonzalez@cc-ygonzalez-mbp.local>
2019-01-14 18:39:44 -0500

    Add GuestList Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import React, { Component } from 'react';
-// import './App.css';
+import './App.css';
+import GuestList from './GuestList'
 
 class App extends Component {
+
+  state = {
+    guests: [
+      {
+        name: 'Treasure',
+        isConfirmed: false
+      },
+      {
+        name: 'Nic',
+        isConfirmed: true
+      },
+      {
+        name: 'Matt K',
+        isConfirmed: false
+      }
+    ]
+  }
+
+  getTotalInvited = () => this.state.guests.length;
+  // getAttendingGuests = () =>
+  // getUnconfirmedGuests= () =>
+
   render() {
     return (
       <div className="App">
@@ -36,32 +59,8 @@ class App extends Component {
               </tr>
             </tbody>
           </table>
-          <ul>
-            <li className="pending"><span>Safia</span></li>
-            <li className="responded"><span>Iver</span>
-              <label>
-                <input type="checkbox" checked /> Confirmed
-              </label>
-              <button>edit</button>
-              <button>remove</button>
-            </li>
-            <li className="responded">
-              <span>Corrina</span>
-              <label>
-                <input type="checkbox" checked /> Confirmed
-              </label>
-              <button>edit</button>
-              <button>remove</button>
-            </li>
-            <li>
-              <span>Joel</span>
-              <label>
-                <input type="checkbox" /> Confirmed
-              </label>
-              <button>edit</button>
-              <button>remove</button>
-            </li>
-          </ul>
+          
+          <GuestList guests={this.state.guests} />
         </div>
     </div>
     );
