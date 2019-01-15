d141760fa9fec4d661337d2d483c611af0a8251b
Yandy Gonzalez <ygonzalez@cc-ygonzalez-mbp.local>
2019-01-15 15:30:48 -0500

    Create a ghost tile while creating new guest

--- src/App.js
+++ src/App.js
@@ -140,6 +140,7 @@ class App extends Component {
             setNameAt={this.setNameAt}
             isFiltered={this.state.isFiltered}
             removeGuestAt={this.removeGuestAt}
+            pendingGuest={this.state.pendingGuest}
           />
         </div>
     </div>
