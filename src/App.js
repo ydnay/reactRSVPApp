7126c0bcd3da0269e16e3100ca1cfae97625716e
Yandy Gonzalez <ygonzalez@cc-ygonzalez-mbp.local>
2019-01-15 16:06:56 -0500

    make the counter component update as the state of the app changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
+
 import GuestList from './GuestList';
+import Counter from './Counter';
 
 class App extends Component {
 
@@ -88,10 +90,16 @@ class App extends Component {
   }  
 
   getTotalInvited = () => this.state.guests.length;
-  // getAttendingGuests = () =>
-  // getUnconfirmedGuests= () =>
+  getAttendingGuests = () =>
+    this.state.guests.reduce(
+      (total, guest) => guest.isConfirmed ? total + 1 : total, 
+      0
+    );
 
   render() {
+    const totalInvited = this.getTotalInvited();
+    const numberAttending = this.getAttendingGuests();
+    const numberUnconfirmed =  totalInvited - numberAttending;
     return (
       <div className="App">
         <header>
@@ -116,22 +124,10 @@ class App extends Component {
                 checked={this.state.isFiltered} /> Hide those who haven't responded
             </label>
           </div>
-          <table className="counter">
-            <tbody>
-              <tr>
-                <td>Attending:</td>
-                <td>2</td>
-              </tr>
-              <tr>
-                <td>Unconfirmed:</td>
-                <td>1</td>
-              </tr>
-              <tr>
-                <td>Total:</td>
-                <td>3</td>
-              </tr>
-            </tbody>
-          </table>
+          <Counter
+            totalInvited={totalInvited}
+            numberAttending={numberAttending}
+            numberUnconfirmed={numberUnconfirmed} />
           
           <GuestList 
             guests={this.state.guests}
